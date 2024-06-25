8c28ba017d4e5766ec2d4950dd127523841a7d53
HimanshuSardana <himanshusardana2005@gmail.com>
2024-06-25 17:46:20 +0530

    Added chapter selection

--- go.mod
+++ go.mod
@@ -35,8 +35,10 @@ require (
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/muesli/ansi v0.0.0-20230316100256-276c6243b2f6 // indirect
 	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.15.2 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/sahilm/fuzzy v0.1.1-0.20230530133925-c48e322e2a8f // indirect
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
 	github.com/temoto/robotstxt v1.1.1 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
--- main.go
+++ main.go
@@ -15,8 +15,33 @@ type Manga struct {
     URL    string
 }
 
+type Chapter struct {
+    Name 	string
+    URL		string
+    UploadDate 	string
+}
+
 func main() {
     var mangas []Manga
+    headers := map[string]string{
+        "Host":                      "chapmanganato.to",
+        "User-Agent":                "Mozilla/5.0 (X11; Linux x86_64; rv:127.0) Gecko/20100101 Firefox/127.0",
+        "Accept":                    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+        "Accept-Language":           "en-US,en;q=0.5",
+        "Accept-Encoding":           "gzip, deflate, br, zstd",
+        "Connection":                "keep-alive",
+        "Referer":                   "https://chapmanganato.to/manga-aa951409",
+        "Cookie":                    "ci_session=Uj%2FMmsw3snmXDdm%2FAxXvAh30dOWXFaZawBkdrdGCA0eWDAxwI77%2FgH%2BU6TRe4RzUikBDsDMIYQTSDN%2FZ8O388NJzeHTdOsCLpCa6MoysPwk0g9fI1ntRO8qn0%2B3zZHWg6%2Be1SrOYgs0KxZU6wS9lo%2F3dej81aq1Vw%2Baz7EBeSsrYnVVqdcATFl7PhnVh65J3QEvJa8bMKCkeXsdyuGJNCOkGpkkCXlemCTNguS%2F71i2qygsuZa5G4XJqTaSBDWN4%2FzhdrcgGhggfc5wtQIsYT1qYEsXcWXcq2J32x%2BnHMiTp%2FSc9rxbm4jvPnaf8tZOG%2FVBcvbOk3ZvrjxVQiG6bY0V6uYmqM4Fm6eWaj5%2Fhik9dwWz5BJISf%2B4lJJJadzg8CcVIJht5ABZAKrytGkFpKtFhHiXFnKqW4HfV%2Fqt9HoY%3Da3468db3c15b1d69fbb668ff5a3f14ce1c5bffc6; panel-fb-comment=fb-comment-title-show",
+        "Upgrade-Insecure-Requests": "1",
+        "Sec-Fetch-Dest":            "document",
+        "Sec-Fetch-Mode":            "navigate",
+        "Sec-Fetch-Site":            "same-origin",
+        "Sec-Fetch-User":            "?1",
+        "Priority":                  "u=1",
+        "TE":                        "trailers",
+    }
+
+
     c := colly.NewCollector()
     baseURL := "https://mangakakalot.com/search/story/"
     var mangaName string
@@ -34,8 +59,6 @@ func main() {
     }
 
     var selectedManga string
-
-    
 
     mangaSlug := strings.Join(strings.Split(mangaName, " "), "_")
 
@@ -85,4 +108,95 @@ func main() {
     if err2 != nil {
 	    log.Fatal(err2)
     }
+
+    var selectedMangaURL string
+    for _, manga := range mangas {
+	if manga.Name == selectedManga {
+		selectedMangaURL = manga.URL
+	}
+    }
+
+    fmt.Println(selectedMangaURL)
+
+    var chapters []Chapter
+    c.OnHTML("li.a-h", func(e *colly.HTMLElement) {
+        chapterName := e.ChildText("a.chapter-name")
+        chapterURL := e.ChildAttr("a.chapter-name", "href")
+        date := e.ChildText("span.chapter-time")
+
+        chapter := Chapter{
+            Name:       chapterName,
+            URL:        chapterURL,
+            UploadDate: date,
+        }
+
+        chapters = append(chapters, chapter)
+    })
+
+    c.OnRequest(func(r *colly.Request) {
+        fmt.Println("Visiting", r.URL)
+    })
+
+    c.Visit(selectedMangaURL)
+
+    var chapter_names []huh.Option[string]
+    for _, chapter := range chapters {
+        chapter_names = append(chapter_names, huh.NewOption(chapter.Name, chapter.Name))
+    }
+
+    var selectedChapter string
+    form3 := huh.NewForm(
+	    huh.NewGroup(
+		    huh.NewSelect[string]().
+		    Title("Chapter:").
+		    Options(chapter_names...).
+		    Value(&selectedChapter),
+	    ),
+    )
+
+    err3 := form3.Run()
+    if err3 != nil {
+	    log.Fatal(err3)
+    }
+
+    var selectedChapterURL string
+
+    for _, chapter := range chapters {
+	if chapter.Name == selectedChapter {
+		selectedChapterURL = chapter.URL
+    	    }
+    }
+
+    var imageURLs []string
+    c.OnHTML("*", func(e *colly.HTMLElement) {
+	    //ImageURL := e.ChildAttr("img","src")
+	    //imageURLs = append(imageURLs, ImageURL)
+	    fmt.Println(e.DOM.Html())
+	    fmt.Println("hello")
+    })
+
+    c.OnResponse(func(r *colly.Response) {
+        fmt.Println("Response Status Code:", r.StatusCode)
+
+        // Check if the request was successful (status code 200)
+        if r.StatusCode == 200 {
+            // Process the response here
+            fmt.Println("Successful response")
+	    fmt.Println("HTML:", r.Headers)
+        } else {
+            fmt.Println("Request failed with status code:", r.StatusCode)
+        }
+    })
+
+    c.OnRequest(func(r *colly.Request) {
+	for key, value := range headers {
+        	r.Headers.Set(key, value)
+        }
+        fmt.Println("Visiting", r.URL)
+    })
+
+
+    selectedChapterURL = "https://chapmanganelo.com/manga-aa88620/chapter-1118"
+    c.Visit(selectedChapterURL)
+    fmt.Println(imageURLs)
 }
